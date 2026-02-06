9c7ebb2c4ead92deaa8cbf914097b403880b3299
Tony Hsu <gosharplite@gmail.com>
2026-02-06 22:41:58 +0800

    Chore: Start next development cycle

--- cmd/tell-me-go/main.go
+++ cmd/tell-me-go/main.go
@@ -12,7 +12,7 @@ import (
 	_ "github.com/gosharplite/tell-me-go/internal/cli/commands/version"
 )
 
-const Version = "1.92.0"
+const Version = "1.93.0-dev"
 
 func main() {
 	app := cli.New(Version)
