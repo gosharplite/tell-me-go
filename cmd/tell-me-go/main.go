4147620412e61cd426ff4cf8b6b2ead236f6545f
Tony Hsu <gosharplite@gmail.com>
2026-02-18 17:26:59 +0800

    Chore: Stabilize version for release v3.2.27

--- cmd/tell-me-go/main.go
+++ cmd/tell-me-go/main.go
@@ -11,7 +11,7 @@ import (
 	"github.com/gosharplite/tell-me-go/internal/cli"
 )
 
-const version = "3.2.27-dev"
+const version = "3.2.27"
 
 func main() {
 	app := cli.New(version, os.Stdin, os.Stdout, os.Stderr)
