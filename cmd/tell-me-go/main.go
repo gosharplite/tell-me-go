62f561d522883473c9f082f8bfde3c0e15f1d628
Tony Hsu <gosharplite@gmail.com>
2026-02-25 10:33:24 +0800

    Chore: Start next development cycle

--- cmd/tell-me-go/main.go
+++ cmd/tell-me-go/main.go
@@ -14,7 +14,7 @@ import (
 	"github.com/gosharplite/tell-me-go/internal/cli"
 )
 
-const version = "3.2.49"
+const version = "3.2.50-dev"
 
 func initTracer() {
 	// Use a global no-op tracer provider so spans are silent by default
