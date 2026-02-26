151b4e297c2afd4dc6d605efe3f57bca0d5885e2
Tony Hsu <gosharplite@gmail.com>
2026-02-26 14:32:02 +0800

    Chore: Start next development cycle

--- cmd/tell-me-go/main.go
+++ cmd/tell-me-go/main.go
@@ -19,7 +19,7 @@ import (
 	"github.com/gosharplite/tell-me-go/internal/cli"
 )
 
-const version = "3.3.4"
+const version = "3.3.5-dev"
 
 func initTracer(ctx context.Context) func(context.Context) error {
 	endpoint := os.Getenv("TELL_ME_TRACE_ENDPOINT")
