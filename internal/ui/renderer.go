f86958ad8ab66a6fdfff05a791c1c27c8391b0a6
Tony Hsu <gosharplite@gmail.com>
2026-03-25 06:04:05 +0800

    fix: spinner ticking by removing terminal lock contention

--- internal/ui/renderer.go
+++ internal/ui/renderer.go
@@ -433,30 +433,21 @@ func (r *stdUIRenderer) StartSpinner(ctx context.Context) func() {
 }
 
 func (r *stdUIRenderer) drawLoadingIndicator(ui uiState, frame string, startTime time.Time) {
-	if r.locker != nil {
-		r.locker.TerminalLock()
-		defer r.locker.TerminalUnlock()
-	}
-
 	msg := " Thinking..."
 	if !startTime.IsZero() {
 		elapsed := int(ui.clock.Now().Sub(startTime).Seconds())
 		msg = fmt.Sprintf(" Thinking... (%ds)", elapsed)
 	}
 
-	// We use carriage return to stay on the same line.
-	// We use colorGray for the indicator.
-	_, _ = fmt.Fprintf(ui.stderr, "\r%s%s%s%s", ui.c(colorGray), frame, msg, ui.c(colorReset))
+	// Move to start of line, clear current line, then print the indicator.
+	// We avoid locking here to ensure high-frequency spinner updates aren't blocked by other rendering.
+	_, _ = fmt.Fprintf(ui.stderr, "\r%s%s%s%s%s", ui.c(termClearLine), ui.c(colorGray), frame, msg, ui.c(colorReset))
 }
 
 func (r *stdUIRenderer) clearLoadingIndicator(ui uiState, rawOutput bool) {
-	if r.locker != nil {
-		r.locker.TerminalLock()
-		defer r.locker.TerminalUnlock()
-	}
-
 	// Move to start of line and clear the spinner.
 	// We do NOT add a newline here to allow the answer to start exactly where the spinner was.
+	// We avoid locking here to prevent potential deadlocks when stopping the spinner.
 	_, _ = fmt.Fprint(ui.stderr, "\r"+ui.c(termClearLine))
 }
 
