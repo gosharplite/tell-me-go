fa0b77859fb4943f7b0428a9a66387d747ff2acf
Tony Hsu <gosharplite@gmail.com>
2026-07-09 15:31:01 +0800

    chore: explicitly discard os.RemoveAll errors in TestMain cleanup

--- internal/tools/analysis/main_test.go
+++ internal/tools/analysis/main_test.go
@@ -10,10 +10,10 @@ func TestMain(m *testing.M) {
 	// Clean up shared temp dirs used by dead_code_deep_test.go
 	// (created via os.MkdirTemp and not tied to any test's t.TempDir).
 	if sharedDeepIdentWS.dir != "" {
-		os.RemoveAll(sharedDeepIdentWS.dir)
+		_ = os.RemoveAll(sharedDeepIdentWS.dir)
 	}
 	if sharedDeepLimWS.dir != "" {
-		os.RemoveAll(sharedDeepLimWS.dir)
+		_ = os.RemoveAll(sharedDeepLimWS.dir)
 	}
 	os.Exit(code)
 }
